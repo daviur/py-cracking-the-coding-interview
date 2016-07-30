c3cf3819c239572e135318ab5c1615a8677397a3
daviur <github@davidurbina.net>
2016-07-30 17:24:22 -0400

    Added as17

--- arrays-and-strings/as17.py
+++ arrays-and-strings/as17.py
@@ -19,3 +19,90 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+import pytest
+
+
+def zero_matrix(matrix):
+    """
+    Time: O(nm)) Space: O(1)
+    """
+    first_column_has_zero = any(True for i in range(len(matrix)) if matrix[i][0] == 0)
+    first_row_has_zero = any(True for j in range(len(matrix[0])) if matrix[0][j] == 0)
+
+    _check_for_zeros(matrix)
+    _zero_matrix_columns(matrix)
+    _zero_matrix_rows(matrix)
+
+    if first_column_has_zero:
+        _zero_first_column(matrix)
+    if first_row_has_zero:
+        _zero_first_row(matrix)
+
+    return matrix
+
+
+def _check_for_zeros(matrix):
+    for i in range(1, len(matrix)):
+        for j in range(1, len(matrix[0])):
+            if matrix[i][j] == 0:
+                matrix[i][0] = 0
+                matrix[0][j] = 0
+
+
+def _zero_matrix_columns(matrix):
+    for i in range(1, len(matrix)):
+        if matrix[i][0] == 0:
+            for j in range(1, len(matrix[0])):
+                matrix[i][j] = 0
+
+
+def _zero_matrix_rows(matrix):
+    for j in range(1, len(matrix[0])):
+        if matrix[0][j] == 0:
+            for i in range(1, len(matrix)):
+                matrix[i][j] = 0
+
+
+def _zero_first_column(matrix):
+    for i in range(len(matrix)):
+        matrix[i][0] = 0
+
+
+def _zero_first_row(matrix):
+    for j in range(len(matrix[0])):
+        matrix[0][j] = 0
+
+
+@pytest.mark.parametrize('input, expected', [
+    ([[0, 1, 1],
+      [1, 0, 1],
+      [1, 1, 0]], [[0, 0, 0],
+                   [0, 0, 0],
+                   [0, 0, 0]]),
+    ([[1, 0, 1, 1],
+      [1, 1, 1, 1],
+      [0, 1, 1, 1]], [[0, 0, 0, 0],
+                      [0, 0, 1, 1],
+                      [0, 0, 0, 0]]),
+    ([[1, 1, 1, 1],
+      [1, 1, 1, 1],
+      [1, 1, 0, 1],
+      [1, 1, 1, 1]], [[1, 1, 0, 1],
+                      [1, 1, 0, 1],
+                      [0, 0, 0, 0],
+                      [1, 1, 0, 1]]),
+    ([[1, 1, 1],
+      [1, 1, 1],
+      [1, 0, 1],
+      [1, 1, 1]], [[1, 0, 1],
+                   [1, 0, 1],
+                   [0, 0, 0],
+                   [1, 0, 1]]),
+    ([[1, 1, 1, 1],
+      [1, 1, 1, 1],
+      [1, 1, 1, 1]], [[1, 1, 1, 1],
+                      [1, 1, 1, 1],
+                      [1, 1, 1, 1]])
+])
+def test_zero_matrix(input, expected):
+    assert zero_matrix(input) == expected
